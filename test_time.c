ef32224b994cb6ee36e5aba85daa152bacb19b8f
Chen Wenlei <dzsdzsdzs00x@163.com>
2017-06-20 07:59:52 +0800

    Add files via upload

---
+++ test_time.c
@@ -0,0 +1,21 @@
+/* test time the code takes */
+
+#include <stdio.h>
+#include <time.h>
+
+int main()
+{
+	int i;
+	double start, end;
+
+	start = (double)clock()/CLOCKS_PER_SEC;
+	for (i=0; i<100000; ++i) {}
+	end = (double)clock()/CLOCKS_PER_SEC; 
+	       
+	printf("%lf s\n", end - start);
+	
+	return 0;
+}
+
+
+
